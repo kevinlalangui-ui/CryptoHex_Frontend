086edae93062000fc4fe45a1b5b88e6852b81d97
Kevin <kevin.dayan.lalangui@estudiante.epsum.school>
2026-05-17 23:55:38 +0200

    cambios environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,9 @@
 export const environment = {
   production: false,
-  apiURL: "http://localhost:8000/api",
-}
+  apiURL: "https://web-production-42fbc0.up.railway.app/api",
+// apiURL: "http://localhost:8000/api",
 
+}
 
 
 
