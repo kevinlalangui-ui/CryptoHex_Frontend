f5a9e4bfd4cffeffc361ed8d77c84068b89358c9
Kevin <kevin.dayan.lalangui@estudiante.epsum.school>
2026-05-18 01:26:00 +0200

    cambios pagprin,menu,

--- src/app/features/pagina-principal/pagina-principal.ts
+++ src/app/features/pagina-principal/pagina-principal.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, signal } from '@angular/core';
 import { DatePipe, NgClass } from '@angular/common';
 import { NewsService, Noticias} from '../../core/services/news/news.service';
+import {AuthCookieService} from '../../core/services/cookies/auth-cookie.service';
+import {SessionStorageService} from '../../core/services/sessions/session-storage.service';
 //penre
 @Component({
   selector: 'app-pagina-principal',
@@ -9,11 +11,19 @@ import { NewsService, Noticias} from '../../core/services/news/news.service';
   styleUrl: './pagina-principal.scss',
 })
 export class PaginaPrincipal implements OnInit {
+  isLoggedIn = signal<boolean>(false);
+  nombreDeUsuario = signal<string | null>(null);
+
   noticias = signal<Noticias[]>([]);
   cargando = signal<boolean>(true);
   error = signal<string | null>(null);
 
-  constructor(private newsService: NewsService) {}
+  constructor(private newsService: NewsService,
+              private cookieService: AuthCookieService ,
+              private sessionService :SessionStorageService) {
+    this.ponerDatos(cookieService, sessionService);
+
+  }
 
   ngOnInit() {
     this.newsService.getNoticias().subscribe({
@@ -36,4 +46,16 @@ export class PaginaPrincipal implements OnInit {
   getSentimientoTexto(sentimiento: boolean): string {
     return sentimiento ? 'Bullish' : 'Bearish';
   }
+  private ponerDatos(cookieService: AuthCookieService, sessionService: SessionStorageService) {
+    if (cookieService.get('cryptoHex_online_token')) {
+      this.isLoggedIn.set(true);
+      let datos = sessionService.get('cryptoHex_datos');
+      if (datos) {
+        this.nombreDeUsuario.set(datos.nombre);
+      }
+
+    }
+  }
 }
+
+
